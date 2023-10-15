2365945fda7901a876867b723158e65459dfc0e3
luthfi9251 <90464220+luthfi9251@users.noreply.github.com>
2023-10-15 09:56:00 +0700

    configuring frontend+backend+socket on one platform

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,28 @@
-const passport = require('passport')
-const express = require('express')
-const router = express.Router()
+const passport = require("passport");
+const express = require("express");
+const router = express.Router();
 
-router.get('/google',passport.authenticate('google', { scope: ['profile','email'] }))
+router.get(
+    "/google",
+    passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-router.get('/redirect/google',passport.authenticate('google', {
-    successRedirect: "/chat",
-    failureRedirect: '/auth/redirect/failed' //please fix this later
-}));
+router.get(
+    "/redirect/google",
+    passport.authenticate("google", {
+        successRedirect:
+            process.env.ENVIRONMENT === "PRODUCTION"
+                ? "/chat"
+                : "http://localhost:5173/chat",
+        failureRedirect: "/auth/redirect/failed", //please fix this later
+    })
+);
 
-router.get('/failure',(req,res)=>{
+router.get("/failure", (req, res) => {
     res.status(401).json({
         success: false,
-        message: "Fail when attemting to login"
-    })
-})
+        message: "Fail when attemting to login",
+    });
+});
 
-
-
-module.exports = router
+module.exports = router;
